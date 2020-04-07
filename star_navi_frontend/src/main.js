4de64ef2cd1e7f9d79ed201e741a1d6a0b48d22f
Oleg <real.trolforever@gmail.com>
2020-04-08 02:04:01 +0300

    Install pkgs, add assets, set up router an main.js

--- star_navi_frontend/src/main.js
+++ star_navi_frontend/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
+import qs from 'querystring';
+import axios from 'axios';
+import VueCookies from 'vue-cookies';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(VueCookies);
+Vue.use(require('vue-moment'));
+
+Vue.prototype.$http = axios;
+Vue.prototype.$qs = qs;
 
 new Vue({
   router,
